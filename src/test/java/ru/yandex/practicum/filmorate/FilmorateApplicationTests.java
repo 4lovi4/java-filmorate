ca8fb10ff7b93b4c902df79b5de2f54cb85748f8
a.a.dyatlov <a.a.dyatlov@tinkoff.ru>
2023-08-28 02:08:58 +0300

    fix: checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,9 +1,7 @@
 package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 class FilmorateApplicationTests {
