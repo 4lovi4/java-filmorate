f254b9617814d0523931635bcd2fcfaed54bf278
a.a.dyatlov <a.a.dyatlov@tinkoff.ru>
2023-08-28 20:05:53 +0300

    feat: Добавил валидацию в классы User Film

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,8 +3,11 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.lang.Nullable;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
 import java.time.LocalDate;
 
 @Data
@@ -13,8 +16,11 @@ public class Film {
     @EqualsAndHashCode.Exclude
     @Nullable
     private Long id;
+    @NotBlank(message = "name не может быть пустым")
     private final String name;
+    @Length(max = 200, message = "описание в поле description не больше 200 символов")
     private String description;
     private final LocalDate releaseDate;
+    @Positive(message = "в поле duration должно быть положительное число")
     private Integer duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -5,6 +5,10 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.lang.Nullable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 @Data
@@ -13,8 +17,12 @@ public class User {
     @EqualsAndHashCode.Exclude
     @Nullable
     private Long id;
+    @NotBlank(message = "Поле email не может быть пустым")
+    @Email(message = "email должен соответствовать паттерну name@domen.zone и содержать @")
     private final String email;
+    @Pattern(regexp = "^\\S+$", message = "login не должен содержать пробелы")
     private String login;
     private String name;
+    @PastOrPresent(message = "Дата рождения не может быть в будущем")
     private LocalDate birthday;
 }
